249fb40359bf8b43186e60df928736f358cab115
sushanthc00 <sushanth15900@gmail.com>
2022-08-08 17:15:14 +0530

    First and Third Task

--- src/TaskThree.java
+++ src/TaskThree.java
@@ -12,15 +12,17 @@ public class TaskThree {
             char b = sc.next().charAt(0);
             for(int i=1;i<=n;i++)
             {
-                for(int j=0;j<n+1-i;j++)
+                for(int j=0;j<i;j++)
                 {
                     System.out.print(a);
                 }
 
-                for(int j=0;j<i;j++)
+                for(int j=0;j<n+1-i;j++)
                 {
                     System.out.print(b);
                 }
+
+
                 System.out.println();
             }
         }
